22055c3f38cb05eb702c458bde06b0858c705a9e
Shubham Agrwal <shubhamagrawal6288@gmail.com>
2026-05-13 12:30:15 +0530

    Initial commit

---
+++ src/main/java/com/example/cabwale/CabwaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabwale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabwaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabwaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cabwale/CabwaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabwale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabwaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
